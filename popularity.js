eb8474aba55b6d57d592c76a0e225d8c9f07b213
Vishnu Tej <vishnutej9492@gmail.com>
2016-04-11 16:38:15 -0700

    Hidden auth API keys

--- popularity.js
+++ popularity.js
@@ -1,7 +1,22 @@
+var request = require('request');
+var https = require('https');
 var google = require ('googleapis');
-google.options ({ auth: 'AIzaSyBIG38r2mMIRw_UXbUVyXnHSua8-2LWCzY' });
+google.options ({ auth: '#KEY' });
 var youtube = google.youtube ('v3');
-var sumCount=0;
+var ytCount=0;
+var twitchCount=0;
+var twitterCount=0;
+var popIndex=0;
+
+if (process.argv.length <= 3) {
+    console.log("Usage: " + __filename + " TwitchHandle TwitterHandle ");
+    process.exit(-1);
+}
+ 
+var twitchHandle = process.argv[2];
+var twitterHandle = process.argv[3];
+ 
+console.log('Parameters: ' + twitchHandle +' '+ twitterHandle);
 
 function search_youtube (query, callback) {
   youtube.search.list (
@@ -31,8 +46,8 @@ function search_youtube (query, callback) {
             if (err2) { return callback (err2); }
             if (data.items.length >= 1) {
               video.viewCount = data.items[0].statistics.viewCount;	
-              sumCount = sumCount + parseInt(data.items[0].statistics.viewCount, 10);
-              callback (video);
+              ytCount = ytCount + parseInt(data.items[0].statistics.viewCount, 10);
+              //callback (video);
             }
           }
         );
@@ -40,8 +55,46 @@ function search_youtube (query, callback) {
     }
   );
 }
-search_youtube ('c9 skadoodle', console.log);
+search_youtube (twitchHandle, console.log);
 setTimeout(function(){
-    console.log('Sum of top 5 video view counts:'+sumCount);
+    console.log('Youtube Top5 Views:'+ytCount);
 },1000);
 
+var http= require("http");
+var url = "http://api.twitch.tv/kraken/channels/"+twitchHandle;
+var request = http.get(url, function (response) {
+    var buffer = "", 
+        data;
+
+    response.on("data", function (chunk) {
+        buffer += chunk;
+    }); 
+
+    response.on("end", function (err) {
+        data = JSON.parse(buffer);
+        twitchCount=data.views;
+        console.log('Twitch Channel Views:'+data.views);
+    }); 
+}); 
+
+var Twitter = require('twitter');
+ 
+var client = new Twitter({
+  consumer_key: '#KEY',
+  consumer_secret: '#KEY',
+  access_token_key: '#KEY',
+  access_token_secret: '#KEY'
+});
+ 
+var params = {screen_name: twitterHandle};
+client.get('users/show', params, function(error, tweets, response){
+  if (!error) {
+  	twitterCount=tweets.followers_count;
+    console.log('Twitter Followers:'+tweets.followers_count);
+  }
+});
+
+setTimeout(function(){
+	var popularityIndex= ((ytCount/5)+twitchCount+twitterCount)/500000;
+    console.log('Popularity Index (out of 100): '+popularityIndex);
+},1400);
